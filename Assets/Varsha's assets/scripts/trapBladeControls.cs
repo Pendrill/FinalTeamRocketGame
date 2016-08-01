7c15183b44a34dc7b0b39ada0e3e6456e33d7370
Ilan Heuman Trencher <iht214@nyu.edu>
2016-08-01 14:39:22 -0400

    trapBlade prototype

---
+++ Assets/Varsha's assets/scripts/trapBladeControls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class prototypeBladeTrap : MonoBehaviour {
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKey(KeyCode.Keypad1)) {
+			transform.Rotate (transform.forward);
+		}
+
+		if (Input.GetKey (KeyCode.Keypad3)) {
+			transform.Rotate (-transform.forward); 
+		}
+
+	}
+}
