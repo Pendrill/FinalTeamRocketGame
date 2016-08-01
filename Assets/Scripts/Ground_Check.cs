6509da3973e1f6629a3f45e795c3b5fa1f259ce1
Omar Dirahoui <od369@nyu.edu>
2016-08-01 14:22:56 -0400

    no message

---
+++ Assets/Scripts/Ground_Check.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground_Check : MonoBehaviour {
+
+    private Player_Controller player;
+
+	// Use this for initialization
+	void Start () {
+        player = gameObject.GetComponentInParent<Player_Controller>();
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D coll) {
+        player.grounded = true;
+
+    }
+    void OnTriggerStay2D(Collider2D coll)
+    {
+        player.grounded = true;
+
+    }
+
+    void OnTriggerExit2D(Collider2D coll) {
+        player.grounded = false;
+
+    }
+	
+	
+}
---
+++ Assets/Scripts/Player_Health.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Health : MonoBehaviour {
+    public int maxHealth = 100;
+    public int currentHealth = 0;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        // everyone starts at 100% health at the start
+        currentHealth = maxHealth;
+
+    }
+
+    // notice we made this a public function; thats so death trigger can use it
+
+    public void Hurt(int damage)
+    {
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
+}
