0e7ad2722644dbf25681611fb520d74b41acd878
Ilan Heuman Trencher <iht214@nyu.edu>
2016-08-03 13:19:34 -0400

    updated TrapBlade, working on disabling button

--- Assets/Scripts/trapBladeControls.cs
+++ Assets/Scripts/trapBladeControls.cs
@@ -1,21 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class prototypeBladeTrap : MonoBehaviour {
+public class trapBladeControls : MonoBehaviour {
 
+	//stops swinging if current time is higher than this time
+	private float stopSwingingAtThisTime = 0.0f;
+	//speed adjustor
+	public float swingSpeed = 1.0f;
 
 
 	// Update is called once per frame
 	void Update () {
 
+	
 
-		if (Input.GetKey(KeyCode.Keypad1)) {
-			transform.Rotate (transform.forward);
+		//if current time is less than the current time +2 seconds, activate
+		if (Time.time < stopSwingingAtThisTime) {
+
+
+
+			transform.Rotate (transform.forward * Time.deltaTime * swingSpeed);
+
+
+		
+
+
 		}
 
-		if (Input.GetKey (KeyCode.Keypad3)) {
-			transform.Rotate (-transform.forward); 
-		}
+		
+
+	}
+
+
+	public void toggle()
+	{
+		//Cooldown timer = current time + 2 seconds
+		stopSwingingAtThisTime = Time.time + 2.0f;
 
 	}
 }
