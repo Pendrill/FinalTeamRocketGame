556fbb08e982da3e3bea66d660c2c0c51329c94e
radiatoryang <campaignjunkie@gmail.com>
2016-08-03 15:45:18 -0400

    Update Ground_Check.cs

--- Assets/Scripts/Ground_Check.cs
+++ Assets/Scripts/Ground_Check.cs
@@ -10,6 +10,12 @@ public class Ground_Check : MonoBehaviour {
         player = gameObject.GetComponentInParent<Player_Controller>();
 	
 	}
+	
+	void Update () {
+		// HIGHLY RECOMMEND replacing your grounded check with a raycast instead of triggers
+		// triggers aren't really the most stable for this
+		// grounded checks are traditionally done with a very short downwards raycast below the player's feet
+	}
 
     void OnTriggerEnter2D(Collider2D coll) {
         player.grounded = true;
