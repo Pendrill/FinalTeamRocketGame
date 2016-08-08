0c20377fb1ddab5ca7564b04a4571a21d1414ee8
Aleksei Waddington <Aleksei Waddington>
2016-08-08 09:15:30 -0400

    As you can see, a lot is being pushed here. In terms of major changes, I have tried to implement as many levels as possible for our monday playtest. Furthermore, I had to update some code to make things work properly. The blade button now deactivates properly. Moreover, the traps will deal damage to the character. I also set up some plce holder menu screens and game over screens and such. I also added a new trap which is the laser beam. There are still two of ryan's stages that need to be done, and the ones already in unity need to be tweaked.

---
+++ Assets/Scripts/SceneNavigation/GoToFirstLevel.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class GoToFirstLevel : MonoBehaviour {
+
+	public void StartLevelOne(){
+		SceneManager.LoadScene (3);
+	}
+}
