070cf85136ac2f367c6450db141186e2ac52c77e
Aleksei Waddington <Aleksei Waddington>
2016-08-11 22:49:24 -0400

    Added the player controller menu. This scene should explain to the player the controls (through animations).

---
+++ Assets/Scripts/BladeScripts/TrapBladeMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TrapBladeMenu : MonoBehaviour {
+
+	//stops swinging if current time is higher than this time
+	private float stopSwingingAtThisTime = 0.0f;
+	//speed adjustor
+	public float swingSpeed = 360.0f;
+	//public Button bladeButton;
+	//public BoxCollider2D bladeBoxCollider2D;
+
+	void Start() {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+			//bladeBoxCollider2D.enabled = true;
+			transform.Rotate (transform.forward * Time.deltaTime * swingSpeed);
+
+
+
+
+
+
+	}
+}
