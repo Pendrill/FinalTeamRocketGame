c49eb032c263a40cb8ba289e7df109fa8a570ed7
Aleksei Waddington <Aleksei Waddington>
2016-08-15 18:18:52 -0400

    Final changes made for the build

---
+++ Assets/Scripts/SceneNavigation/ExitGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Escape)) {
+			Application.Quit ();
+		}
+	}
+}
